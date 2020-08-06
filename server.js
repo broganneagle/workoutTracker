f110a6c5dc970ef2110a32dee23a3650c6a2396d
Brogan Neagle <broganneagle12@gmail.com>
2020-08-06 15:06:34 -0500

    server and json commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+const db = require("./models");
+
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
+
+app.listen(PORT, () => {
+  console.log(`running on ${PORT}`);
+});
